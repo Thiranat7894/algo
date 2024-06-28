9b6354c648850b195503270981f23dab005ed71b
Thiranat7894 <138641542+Thiranat7894@users.noreply.github.com>
2024-06-28 09:36:11 +0700

    add 3 file

---
+++ DisplayText.java
@@ -0,0 +1,8 @@
+public class DisplayText {
+    public static void main(String[] args) {
+        System.out.println("Thiranat" + "Nutcharoan");
+        System.out.println("10" + 5);
+        System.out.println("NPRU" + 2567);
+        System.out.println("3" + 2);
+    }
+}
